8c559876f4a368f2011c7f5bf7c8c43b51937e30
Bratislav <bratislav.2000@gmail.com>
2024-01-08 21:05:40 +0100

    Narpravljene kolekcije za prikaz podataka

---
+++ app/Http/Resources/ReservationResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ReservationResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+
+    public static $wrap = 'Reservation';
+    public function toArray($request)
+    {
+        //return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'user' => new UserResource($this->user),
+            'room' => new RoomResource($this->room),
+            'date' => $this->reserved_date,
+            'status' => $this->status
+        ];
+    }
+}
