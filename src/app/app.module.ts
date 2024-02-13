7d21f3fcbac8bdfc99ba04909088b11af32f80fa
Bratislav <bratislav.2000@gmail.com>
2024-02-13 14:34:28 +0100

    Inicijalni commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { RoomsComponent } from './components/rooms/rooms.component';
+import { RoomListComponent } from './components/rooms/room-list/room-list.component';
+import { RoomDetailComponent } from './components/rooms/room-detail/room-detail.component';
+import { RoomItemComponent } from './components/rooms/room-list/room-item/room-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RoomsComponent,
+    RoomListComponent,
+    RoomDetailComponent,
+    RoomItemComponent,
   ],
   imports: [
     BrowserModule
